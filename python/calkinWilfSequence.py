92805c29053bd8de4aea0655f59699a103e4bb39
stikcy-ai <rev.hyeok@gmail.com>
2020-03-30 22:16:11 +0900

    First commit

---
+++ python/calkinWilfSequence.py
@@ -0,0 +1,13 @@
+def calkinWilfSequence(number):
+    def fractions():
+        a, b = 1, 1
+        while True:
+            yield [a, b]
+            a, b = b, a - 2 * (a % b) + b
+
+    gen = fractions()
+    res = 0
+    while next(gen) != number:
+        res += 1
+    return res
+
