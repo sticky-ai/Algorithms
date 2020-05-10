0317af7a9082ffe7d04f419a1827cd461da68cfa
stikcy-ai <rev.hyeok@gmail.com>
2020-05-10 23:39:11 +0900

    Modify codes

--- arcade/the_core/crosswordFormation.py
+++ arcade/the_core/crosswordFormation.py
@@ -1,26 +1,14 @@
-from itertools import permutations
-
 def crosswordFormation(words):
-    top, left, right, bottom = [i for i in words]
-    cnt = 0
-
-    for i in range(len(top)):
-        for j in range(len(left)):
-            for k in range(i + 2, len(top)):
-                for l in range(len(right)):
-                    offset = abs(i - k)
-                    
-                    if top[i] != left[j] or top[k] != right[l]:
-                        continue
-                    
-                    for m in range(2, len(left)):
-                        left_bottom = left[j + m]
-                        right_bottom = right[l + m]
-
-                        if not left_bottom or not right_bottom:
-                            break
-                        
-                        for n in range(len(bottom) - offset):
-                            if left_bottom == bottom[n] and right_bottom == bottom[n + offset]:
-                                cnt += 1
-    return cnt
+    from itertools import permutations as p
+    c = 0
+    for l in p(words):
+        for d1 in range(len(l[0])-2):
+            s1 = [i for i,j in enumerate(l[1]) if j == l[0][d1]]
+            for d2 in range(d1+2, len(l[0])):
+                s2 = [i for i,j in enumerate(l[2]) if j == l[0][d2]]
+                for cw1 in s1:
+                    for cw2 in s2:
+                        for c1, c2 in zip(l[1][cw1+2:],l[2][cw2+2:]):
+                            for c3, c4 in zip(l[3],l[3][d2-d1:]):
+                                c += c1 == c3 and c2 == c4
+    return c
