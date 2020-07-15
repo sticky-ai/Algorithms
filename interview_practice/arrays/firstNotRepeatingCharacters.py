bc3060d2e092253d6b9147b717205fc337993324
stikcy-ai <rev.hyeok@gmail.com>
2020-07-15 12:03:49 +0900

    First commit

--- interview_practice/arrays/firstNotRepeatingCharacters.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-def firstNotRepeatingCharacter(s):
-    res = [k for k, v in Counter(s).most_common() if v == 1]
-    if len(res) == 0:
-        return '_'
-    else:
-        return res[0]
--- test_center/mutateTheArray.py
+++
@@ -1,5 +0,0 @@
-def mutateTheArray(n, a):
-    a.insert(0, 0)
-    a.append(0)
-    return [sum(a[i:i+3]) for i in range(n) if len(a[i:i+3]) == 3]
-    
