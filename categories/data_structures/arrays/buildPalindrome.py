ec5d97755228b35d99ac6e32aac57bdb31e44250
stikcy-ai <rev.hyeok@gmail.com>
2020-07-24 19:25:10 +0900

    First commit

--- categories/data_structures/arrays/buildPalindrome.py
+++ categories/data_structures/arrays/buildPalindrome.py
@@ -1,4 +1,10 @@
 def buildPalindrome(st):
+    if st == st[::-1]: return st
+    
+    for i in range(len(st) + 1): 
+        temp = st + st[:i][::-1] 
+        if temp == temp[::-1]:
+            return tempdef buildPalindrome(st):
     l = len(st)
     st = list(st)
     for v in st:
