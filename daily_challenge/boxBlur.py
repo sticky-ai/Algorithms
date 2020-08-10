6cba019f6a8c4b0e356d318e2d7893491f672546
stikcy-ai <rev.hyeok@gmail.com>
2020-08-10 14:57:02 +0900

    Add simplified code

--- daily_challenge/boxBlur.py
+++ daily_challenge/boxBlur.py
@@ -6,3 +6,10 @@ def boxBlur(image):
         for j in range(len(image[0]) - 2):
             res.append(sum(sum(m) for m in image[i:i+3, j:j+3]) // 9)
     return np.reshape(res, (len(image) - 2, len(image[0]) - 2))
+
+#def boxBlur(image):
+#    return [[sum(sum(x[i:i+3]) for x in image[j:j+3])//9 
+#                                   for i in range(len(image[0])-2)] 
+#                                   for j in range(len(image)-2)]
+
+
