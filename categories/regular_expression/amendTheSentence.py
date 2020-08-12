c56cfbf67aeb70ee16f86c3012c703fd95f6541f
stikcy-ai <rev.hyeok@gmail.com>
2020-08-12 11:43:12 +0900

    First commit

--- categories/regular_expression/amendTheSentence.py
+++ categories/regular_expression/amendTheSentence.py
@@ -2,3 +2,7 @@ def amendTheSentence(s):
     f = lambda x: ' ' + x.group().lower()
     p = re.compile('[A-Z]')
     return p.sub(f, s).lstrip()
+
+# def amendTheSentence(s):
+#     sp = [n for n in re.split('([A-Z]{1}[a-z]*)', s) if n != '']
+#     return ' '.join(sp).lower()
