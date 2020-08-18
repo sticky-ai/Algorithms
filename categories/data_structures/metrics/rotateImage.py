9f1552c32b7b8a0ef70dcd97c9a85ae02f111486
stikcy-ai <rev.hyeok@gmail.com>
2020-08-18 16:07:59 +0900

    Update simplified code

--- categories/data_structures/metrics/rotateImage.py
+++ categories/data_structures/metrics/rotateImage.py
@@ -1,7 +1,11 @@
-import numpy as np
-
 def rotateImage(a):
-    t = np.transpose(a)
-    return [i[::-1] for i in t]
+    return list(zip(*a[::-1]))
+
+
+#import numpy as np
+#
+#def rotateImage(a):
+#    t = np.transpose(a)
+#    return [i[::-1] for i in t]
 
 
