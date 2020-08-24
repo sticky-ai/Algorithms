4aae94f5cec6f11edca7fdb01998df65dd69656f
stikcy-ai <rev.hyeok@gmail.com>
2020-08-24 15:06:59 +0900

    First commit

--- categories/data_structures/arrays/composeRanges.py
+++ categories/data_structures/arrays/composeRanges.py
@@ -1,12 +1,13 @@
 def composeRanges(nums):
     if not nums: 
         return []
-    s, e, r = nums[0], nums[0], []
-    for x in nums[1:]:
-        if x - e != 1:
-            r.append(f'{s}->{e}' if s != e else str(s))
-            s = x
-        e = x
-    r.append(f'{s}->{e}' if s != e else str(s))
-    return r
-
+    
+    start, end, res = nums[0], nums[0], []
+    for n in nums[1:]:
+        if n - end != 1:
+            res.append(f'{start}->{end}' if start != end else str(start))
+            start = n
+        end = n
+    res.append(f'{start}->{end}' if start != end else str(start))
+    return res
+    
