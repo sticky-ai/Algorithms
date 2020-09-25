a7dad8ae0abb7bcfd0a89e9e835255bf762c6243
stikcy-ai <rev.hyeok@gmail.com>
2020-09-25 12:25:59 +0900

    Update code

--- codesignal/daily_challenge/catalogUpdate.py
+++ codesignal/daily_challenge/catalogUpdate.py
@@ -1,13 +1,25 @@
-# 178 chars
-import collections
+# 156 chars
 c, u = eval(dir()[0])
-d = collections.defaultdict(list)
-for i in c:
-    d[i[0]] = i[1:]
+d = {i[0]:i[1:] for i in c}
 for j in u:
-    if j[1] not in d[j[0]]:
-        d[j[0]].append(j[1])
+    k, v = j[0], j[1]
+    if k not in d.keys():
+        d[k] = [v]
+    if v not in d[k]:
+        d[k].append(v)
 return sorted([k] + sorted(v) for k, v in d.items())
+
+
+# 178 chars
+# import collections
+# c, u = eval(dir()[0])
+# d = collections.defaultdict(list)
+# for i in c:
+#     d[i[0]] = i[1:]
+# for j in u:
+#     if j[1] not in d[j[0]]:
+#         d[j[0]].append(j[1])
+# return sorted([k] + sorted(v) for k, v in d.items())
 
 # 208 chars
 # import collections
