e515df2b1461e3f383a4e93cd394c28d315ad15b
stikcy-ai <rev.hyeok@gmail.com>
2020-10-13 19:15:18 +0900

    Update code

--- categories/mathematics/visiblePoints.py
+++ categories/mathematics/visiblePoints.py
@@ -1,10 +1,26 @@
+from math import atan2, pi
+
 def visiblePoints(points):
-    cnt = 0
-    minTheta = int(math.degrees(math.atan2(1, 5)))
-    maxTheta = int(math.degrees(math.atan2(3, 2)))
     
-    for x, y in points:
-        theta = abs(int(math.degrees(math.atan2(x, y))))
-        if minTheta <= theta <= maxTheta:
-            cnt += 1
-    return cnt
+    maxView = 0
+    field_width = 2 * pi * (45.0001 / 360)
+
+    angles = [atan2(y, x) % (2 * pi) for x, y in points]
+    angles.sort()
+
+    for x in angles:
+        if x < field_width:
+            angles.append(x + (2 * pi))
+        else:
+            break
+    
+    for i, start_angle in enumerate(angles):
+        for j in range(i, len(angles)):
+            if angles[j] - angles[i] >= field_width:
+                break
+                
+        temp = range(i, j)
+        if len(temp) > maxView:
+            maxView = len(temp)
+
+    return maxView
