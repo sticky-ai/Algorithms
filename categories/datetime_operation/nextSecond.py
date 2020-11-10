1dc37560f5380c9e8b385e9fd2e09ca7054ba519
stikcy-ai <rev.hyeok@gmail.com>
2020-11-10 15:11:24 +0900

    Update hard coding version

--- categories/datetime_operation/nextSecond.py
+++ categories/datetime_operation/nextSecond.py
@@ -4,3 +4,22 @@ def nextSecond(currentTime):
     h, m, s = currentTime
     op = datetime(1, 1, 1, hour=h, minute=m, second=s) + timedelta(seconds=1)
     return [op.hour, op.minute, op.second]
+
+# Hard Coding Version
+def nextSecond(currentTime):
+
+    if currentTime[2] == 59:
+        currentTime[2] = 0
+        if currentTime[1] == 59:
+            currentTime[1] = 0
+            if currentTime[0] == 23:
+                currentTime[0] = 0
+            else:
+                currentTime[0] += 1
+        else:
+            currentTime[1] += 1
+    else:
+        currentTime[2] += 1
+
+    return currentTime
+
