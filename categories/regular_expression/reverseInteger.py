a65ee24ac43c05674de2f21857cc106a85aaebd4
stikcy-ai <rev.hyeok@gmail.com>
2020-12-03 12:27:45 +0900

    Modify code

--- categories/regular_expression/reverseInteger.py
+++ categories/regular_expression/reverseInteger.py
@@ -1,4 +1,4 @@
 def reverseInteger(x):
-    op = re.findall('\W', str(x))
-    nums = re.findall('[0-9]+', str(x))[0]
-    return int(nums[::-1]) if len(op) == 0 else int(op[0] + str(nums[::-1]))
+    op = '+' if len(re.findall('\W', str(x))) == 0 else '-'
+    num = re.findall('\d+', str(x))[0]
+    return int(op + num[::-1])
