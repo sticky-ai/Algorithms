9b95d8b7427905de70c498900399e491bc33a738
stikcy-ai <rev.hyeok@gmail.com>
2020-12-15 16:15:45 +0900

    Modify code

--- categories/data_structures/arrays/sorting_searching/countSumOfTwoRepresentations2.py
+++ categories/data_structures/arrays/sorting_searching/countSumOfTwoRepresentations2.py
@@ -1,12 +1,6 @@
-from itertools import product
-
-def countSumOfTwoRepresentations2(n, l, r):    
+def countSumOfTwoRepresentations2(n, l, r):
     s = set()
-    nums = list(range(l, r+1))
-    for p in product(nums, nums):
-        p = list(p)
-        p.sort()
-        if sum(p) == n:
-            s.add(tuple(p))
-    
+    for i in range(l, r+1):
+        if n - i in range(l, r+1):
+            s.add(tuple(sorted([i, n - i])))
     return len(s)
