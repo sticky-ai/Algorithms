f99fd97d26e544185192a696243a99309de0b7e3
stikcy-ai <rev.hyeok@gmail.com>
2020-12-18 11:23:36 +0900

    Simplify code

--- categories/string/removeDuplicateCharacters.py
+++ categories/string/removeDuplicateCharacters.py
@@ -1,3 +1,7 @@
 def removeDuplicateCharacters(str):
-    e = [s for s in set(str) if str.count(s) > 1]
-    return ''.join([s for s in str if s not in e])
+    return ''.join([s for s in str if str.count(s) < 2])
+
+
+#def removeDuplicateCharacters(str):
+#    e = [s for s in set(str) if str.count(s) > 1]
+#    return ''.join([s for s in str if s not in e])
