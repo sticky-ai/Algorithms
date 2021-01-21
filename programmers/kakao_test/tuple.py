0e3f030ef2a5824fc02a1b9f66eeca0b9ca64530
stikcy-ai <rev.hyeok@gmail.com>
2021-01-21 23:40:54 +0900

    First commit

---
+++ programmers/kakao_test/tuple.py
@@ -0,0 +1,7 @@
+def solution(s):
+
+    s = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+
+import re
+from collections import Counter
