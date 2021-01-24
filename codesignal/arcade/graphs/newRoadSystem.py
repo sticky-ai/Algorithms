88fde5938a1a0cbb98961890cfe699a5e629e751
stikcy-ai <rev.hyeok@gmail.com>
2021-01-24 23:45:55 +0900

    Add another code

--- codesignal/arcade/graphs/newRoadSystem.py
+++ codesignal/arcade/graphs/newRoadSystem.py
@@ -2,3 +2,17 @@ def newRoadSystem(roadRegister):
     check_1 = [i.count(True) for i in roadRegister]
     check_2 = [i.count(True) for i in zip(*roadRegister)]
     return check_1 == check_2
+
+# def newRoadSystem(roadRegister):
+#     d = dict()
+    
+#     for i in range(len(roadRegister)):
+#         d[i] = [0, 0]
+    
+#     for i in range(len(roadRegister)):
+#         for j in range(len(roadRegister)):
+#             if i != j and roadRegister[i][j]:
+#                 d[i][0] += 1
+#                 d[j][1] += 1
+    
+#     return all([r == c for r, c in d.values()])
